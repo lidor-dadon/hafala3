9373a95e0592a4e4930f9beed79a1b4d86248a17
AnatG <anat.gindin@campus.technion.ac.il>
2024-07-31 17:57:51 +0300

    added queue.h

---
+++ queue.h
@@ -0,0 +1,36 @@
+//
+// Created by AnatG on 7/31/2024.
+//
+
+#ifndef HAFALA3C_QUEUE_H
+#define HAFALA3C_QUEUE_H
+
+enum Schedalg {BLOCK, DT, DH, BF, RANDOM};
+char* schedalgArr[] = {"block", "dt", "dh", "bf", "random"};
+
+typedef struct myRequest{
+    int fd;
+    double arrivalTime; //maybe struct timeval?
+                        //use Time_GetSeconds() from output.c
+} myRequest;
+
+typedef struct requestNode{
+    myRequest* myRequest;
+    struct requestNode* prev;
+    struct requestNode* next;
+}requestNode;
+
+
+typedef struct requestQueue{
+    int size;
+    requestNode * head;
+    requestNode * tail;
+} requestQueue;
+
+void push(myRequest* request);
+myRequest * popHead();
+myRequest * popFromIndex();
+myRequest * popTail();
+int getSize();
+
+#endif //HAFALA3C_QUEUE_H
