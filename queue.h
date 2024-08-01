ed7cb42ba435c0d66c03c2d8ea3f05ac36f4318e
Lidor Dadon <102687076+lidor-dadon@users.noreply.github.com>
2024-08-01 12:55:37 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,16 +1,15 @@
-//
-// Created by AnatG on 7/31/2024.
-//
-
 #ifndef HAFALA3C_QUEUE_H
 #define HAFALA3C_QUEUE_H
+#include <stdlib.h>
+#include <sys/time.h>
+
 
 enum Schedalg {BLOCK = 0, DT = 1, DH = 2, BF = 3, RANDOM = 4, NOT_DEFINED};
 char* schedalgArr[] = {"block", "dt", "dh", "bf", "random"};
 
 typedef struct myRequest{
     int fd;
-    double arrivalTime; //maybe struct timeval?
+    struct timeval arrivalTime;
                         //use Time_GetSeconds() from output.c
 } myRequest;
 
@@ -18,19 +17,22 @@ typedef struct requestNode{
     myRequest* myRequest;
     struct requestNode* prev;
     struct requestNode* next;
-}requestNode;
+} requestNode;
 
 
 typedef struct requestQueue{
     int size;
+    int maxSize;
     requestNode * head;
     requestNode * tail;
 } requestQueue;
 
-void push(myRequest* request);
-myRequest * popHead();
-myRequest * popFromIndex();
-myRequest * popTail();
-int getSize();
+requestNode* initRequestNode(myRequest* request);
+void push(requestQueue* queue, myRequest* request);
+myRequest * popHead(requestQueue* queue);
+myRequest * popFromIndex(requestQueue* queue, int index);
+myRequest * popTail(requestQueue* queue);
+int getSize(requestQueue* queue);
+void deleteQueue(requestQueue* queue);
 
 #endif //HAFALA3C_QUEUE_H
