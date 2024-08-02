fbc8b53a775fed29c200e9af9df66effb3e76ccd
Lidor Dadon <102687076+lidor-dadon@users.noreply.github.com>
2024-08-02 12:26:01 +0300

    Update request.h

--- request.h
+++ request.h
@@ -1,5 +1,5 @@
 #ifndef __REQUEST_H__
 
-void requestHandle(int fd, pThread currentThread, struct timeval arriveTime, struct timeval dispatchTime);
+void requestHandle(int fd, struct timeval arrival, struct timeval dispatch, threads_stats t_stats);
 
 #endif
